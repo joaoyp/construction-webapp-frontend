8dcdc31e7f70005b6abc06bc8d2f6131659fac49
João Yp <joaoyp.dev@gmail.com>
2023-12-28 17:07:25 +0000

    WIP Contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -12,13 +12,14 @@ export const Contacts = () => {
 
   return (
     <>
-      <div className="flex bg-red-200 min-real-screen">
+      <div className="flex min-real-screen">
         <div className="w-full flex flex-col">
-          <div className="mt-8 flex w-full justify-center">
-            <Lettering text={"Contactos"} fontSize={50} color={"orange"} />
+          <div className="absolute h-1/3 w-1/3 -mt-40 -ml-60 bg-orange-400 rounded-full"></div>
+          <div className="absolute mt-8 ml-6">
+            <Lettering text={"Contactos"} fontSize={50} color={"white"} />
           </div>
-          <div className="flex w-full justify-center">
-            <div className="mt-4 flex flex-col gap-8 w-2/3">
+          <div className="pl-6 flex w-full justify-center">
+            <div className="mt-56 flex flex-col gap-8 w-3/4">
               <Lettering
                 text={"Tem alguma dúvida ou pretende  um orçamento?"}
                 fontSize={24}
@@ -33,77 +34,38 @@ export const Contacts = () => {
               />
             </div>
           </div>
-          <div className="w-2/3 mt-12 flex flex-col items-center">
-            <div className="flex flex-col w-fit">
-              <div className="flex items-center gap-2">
+          <div className="pl-6 mt-24 w-full flex flex-col items-center">
+            <div className="flex flex-col w-3/4">
+              <Lettering fontSize={24} text={"Telefone:"} />
+              <div className="flex items-center gap-2 mb-8">
                 <PhoneIcon sx={{ color: "orange", fontSize: "28px" }} />
                 <Lettering
-                  text={"+351 966 420 506"}
+                  text={"+351 966 420 506 / +351 96x xxx xxx"}
                   fontSize={22}
                   color={"black"}
                 />
               </div>
+              <Lettering fontSize={24} text={"Email:"} />
               <div className="flex items-center gap-2">
                 <EmailIcon sx={{ color: "orange", fontSize: "28px" }} />
-                <Lettering
-                  text={"pa-construcoes@gmail.com"}
-                  fontSize={22}
-                  color={"black"}
-                />
+                <u>
+                  <Lettering
+                    text={"pa-construcoes@gmail.com"}
+                    fontSize={22}
+                    color={"black"}
+                  />
+                </u>
               </div>
             </div>
           </div>
         </div>
-        <div className="w-full flex items-center">
+        <div className="w-full flex items-center bg-yellow-200">
           <form
             action="POST"
             onSubmit={() => {}}
             method="POST"
             className="bg-blue-200"
-          >
-            <div className="pt-0 mb-3">
-              <input
-                type="text"
-                placeholder="Your name"
-                name="name"
-                className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
-                required
-              />
-            </div>
-
-            <div className="pt-0 mb-3">
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                className="focus:outline-none focus:ring relative w-full px-3 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
-                required
-              />
-            </div>
-
-            <div className="pt-0 mb-3">
-              <textarea
-                placeholder="Your message"
-                name="message"
-                className=""
-                required
-              />
-            </div>
-
-            <div className="pt-0 mb-3">
-              <PrimaryButton
-                type={"submit"}
-                text={"Enviar"}
-                bgcolor={"orange"}
-                color={"white"}
-                hoverBgColor={"white"}
-                hoverColor={"orange"}
-                size={"large"}
-                variant={"contained"}
-                fontFamily={"Alice"}
-              />
-            </div>
-          </form>
+          ></form>
         </div>
       </div>
     </>
